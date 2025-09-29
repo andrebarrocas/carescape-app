9858977c2e4d1dc1eb3882e24bc9929f91e2646b
ist198844 <53345475+andrebarrocas@users.noreply.github.com>
2025-09-29 13:58:30 +0100

    upload images

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -315,7 +315,7 @@ export default function Map({ colors, titleColor, onColorSelect, selectedColorFo
     return new Promise((resolve) => {
       const canvas = document.createElement('canvas');
       const ctx = canvas.getContext('2d');
-      const img = new Image();
+      const img = document.createElement('img');
       
       img.onload = () => {
         // Calculate new dimensions
